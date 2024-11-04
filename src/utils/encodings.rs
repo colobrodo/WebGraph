68af4fb6e56b627796e90bb2a72cb399f29da2e0
colobrodo <colognid@gmail.com>
2024-11-04 15:12:40 +0100

    changed i, j, k parameter for hybrid encoding to be consistent with the ones in the paper

--- src/utils/encodings.rs
+++ src/utils/encodings.rs
@@ -1,4 +1,7 @@
-use crate::bitstreams::{BinaryReader, BinaryWriter, tables::{GAMMAS, ZETAS_3, DELTAS}};
+use crate::bitstreams::{
+    tables::{DELTAS, GAMMAS, ZETAS_3},
+    BinaryReader, BinaryWriter,
+};
 use crate::utils::EncodingType;
 
 pub trait UniversalCode {
@@ -19,7 +22,7 @@ impl UniversalCode for UnaryCode {
         }
 
         let mut x = u32::leading_zeros((reader.current as u32) << (32 - reader.fill));
-        if x < reader.fill as u32{
+        if x < reader.fill as u32 {
             reader.read_bits += x as usize + 1;
             reader.fill -= x as usize + 1;
             return x as u64;
@@ -58,11 +61,11 @@ impl UniversalCode for UnaryCode {
         writer.write(writer.current);
         writer.free = 8;
         writer.current = 0;
-        
+
         let mut i = x >> 3;
 
         writer.written_bits += x as usize & 0x7FFFFFF8;
-        
+
         while i != 0 {
             writer.write(0);
             i -= 1;
@@ -86,7 +89,7 @@ impl UniversalCode for GammaCode {
     fn read_next(reader: &mut BinaryReader, _zk: Option<u64>) -> u64 {
         if reader.fill >= 16 || reader.refill() >= 16 {
             let precomp = GAMMAS[reader.current as usize >> (reader.fill - 16) & 0xFFFF];
-    
+
             if precomp.1 != 0 {
                 reader.read_bits += precomp.1 as usize;
                 reader.fill -= precomp.1 as usize;
@@ -123,11 +126,11 @@ impl UniversalCode for DeltaCode {
     fn read_next(reader: &mut BinaryReader, _zk: Option<u64>) -> u64 {
         if reader.fill >= 16 || reader.refill() >= 16 {
             let precomp = DELTAS[reader.current as usize >> (reader.fill - 16) & 0xFFFF];
-    
+
             if precomp.1 != 0 {
                 reader.read_bits += precomp.1 as usize;
                 reader.fill -= precomp.1 as usize;
-    
+
                 return precomp.0 as u64;
             }
         }
@@ -140,7 +143,7 @@ impl UniversalCode for DeltaCode {
         debug_assert!(x < u64::MAX);
         // if x < MAX_PRECOMPUTED TODO
 
-        let x =  x + 1; // Code [0, +inf - 1]
+        let x = x + 1; // Code [0, +inf - 1]
         let msb = (u64::BITS - 1 - x.leading_zeros()) as u64;
         GammaCode::write_next(writer, msb, None) + writer.push_bits(x, msb)
     }
@@ -161,7 +164,7 @@ impl UniversalCode for ZetaCode {
 
         if zk == 3 && (reader.fill >= 16 || reader.refill() >= 16) {
             let precomp = ZETAS_3[reader.current as usize >> (reader.fill - 16) & 0xFFFF];
-    
+
             if precomp.1 != 0 {
                 reader.read_bits += precomp.1 as usize;
                 reader.fill -= precomp.1 as usize;
@@ -169,11 +172,15 @@ impl UniversalCode for ZetaCode {
                 return precomp.0 as u64;
             }
         }
-    
+
         let unary = UnaryCode::read_next(reader, None);
         let left = 1 << (unary * zk);
         let m = reader.read_int(unary * zk + zk - 1);
-        if m < left {m + left - 1} else {(m << 1) + reader.read_from_current(1) - 1}
+        if m < left {
+            m + left - 1
+        } else {
+            (m << 1) + reader.read_from_current(1) - 1
+        }
     }
 
     #[inline(always)]
@@ -187,11 +194,12 @@ impl UniversalCode for ZetaCode {
         let h = msb / zk;
         let unary = UnaryCode::write_next(writer, h, None);
         let left = 1 << (h * zk);
-        unary + 
-            if x - left < left 
-                {writer.push_bits(x - left, h * zk + zk - 1)}
-            else 
-                {writer.push_bits(x, h * zk + zk)}
+        unary
+            + if x - left < left {
+                writer.push_bits(x - left, h * zk + zk - 1)
+            } else {
+                writer.push_bits(x, h * zk + zk)
+            }
     }
 
     #[inline(always)]
@@ -216,11 +224,22 @@ impl Huffman for Huff {
 /*************** Zuckerli encoding follows ***************/
 
 pub const K_ZUCK: usize = 4;
-pub const I_ZUCK: usize = 2;
-pub const J_ZUCK: usize = 1;
+// pub const I_ZUCK: usize = 2;
+// pub const J_ZUCK: usize = 1;
+// NOTE: replace i and j parameter to be consistent with
+//      the ones in the paper
+pub const I_ZUCK: usize = 1;
+pub const J_ZUCK: usize = 0;
 
 #[inline(always)]
-pub fn zuck_decode(token: usize, reader: &mut BinaryReader, k: usize, msb_in_token /* (i) */: usize, lsb_in_token /* (j) */: usize) -> usize { // 4 2 1
+pub fn zuck_decode(
+    token: usize,
+    reader: &mut BinaryReader,
+    k: usize,
+    msb_in_token /* (i) */: usize,
+    lsb_in_token /* (j) */: usize,
+) -> usize {
+    // 4 2 1
     debug_assert!(k >= lsb_in_token + msb_in_token);
     let split_token = 1 << k; // 2^k
 
@@ -228,21 +247,24 @@ pub fn zuck_decode(token: usize, reader: &mut BinaryReader, k: usize, msb_in_tok
         return token;
     }
 
-    let nbits = k - (msb_in_token + lsb_in_token) +
-                        ((token - split_token) >> (msb_in_token + lsb_in_token));
+    let nbits = k - (msb_in_token + lsb_in_token)
+        + ((token - split_token) >> (msb_in_token + lsb_in_token));
     let low = token & ((1 << lsb_in_token) - 1);
     let token = token >> lsb_in_token;
 
     let bits = reader.read_int(nbits as u64) as usize;
-    (((((1 << msb_in_token) | (token & ((1 << msb_in_token) - 1)))
-        << nbits) |
-        bits)
-        << lsb_in_token) |
-        low
+    (((((1 << msb_in_token) | (token & ((1 << msb_in_token) - 1))) << nbits) | bits)
+        << lsb_in_token)
+        | low
 }
 
 #[inline(always)]
-pub fn zuck_encode(value: usize, k: usize, msb_in_token /* (i) */: usize, lsb_in_token /* (j) */: usize) -> (usize, usize, usize) {
+pub fn zuck_encode(
+    value: usize,
+    k: usize,
+    msb_in_token /* (i) */: usize,
+    lsb_in_token /* (j) */: usize,
+) -> (usize, usize, usize) {
     debug_assert!(k >= lsb_in_token + msb_in_token);
     let split_token = 1 << k; // 2^k
 
@@ -256,7 +278,7 @@ pub fn zuck_encode(value: usize, k: usize, msb_in_token /* (i) */: usize, lsb_in
     let s = split_token +                            // 2^k +
         ((n - k) << (msb_in_token + lsb_in_token)) +        // (p - k - 1) * 2^(i+j) +
         ((m >> (n - msb_in_token)) << lsb_in_token) +       // m * 2^j +
-        (m & ((1 << lsb_in_token) - 1));                    // l
+        (m & ((1 << lsb_in_token) - 1)); // l
     let t_len = n - msb_in_token - lsb_in_token;
     let t = (value >> lsb_in_token) & ((1 << t_len) - 1);
     (s, t_len, t)
